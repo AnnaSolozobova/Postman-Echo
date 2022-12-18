7fb8d372de10d3662c9f916e1e79dd8c67b41b19
AnnaSolozobova <solozobovaanna5@gmail.com>
2022-12-18 03:48:19 +0300

    update test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -16,7 +16,7 @@ public class PostmanTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(300)
+                .statusCode(200)
                 .body("data", equalTo("some data"))
         ;
     }
